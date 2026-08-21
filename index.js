70ef1328e9ba2510d4e4eb7f07478922760c3af7
jameelsikandar <jameelsikandarr@gmail.com>
2026-08-21 09:35:27 +0500

    Stage 4: CRUD API full ready working

--- index.js
+++ index.js
@@ -78,7 +78,48 @@ app.post('/tasks', (req, res) => {
 
 
 
+// put api
+app.put('/tasks/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const task = tasks.find(t => t.id === id);
 
+    if (!task) {
+        return res.status(404).json({
+            error: `Task ${id} not found`
+        });
+    }
+
+    const { title, done } = req.body;
+
+    if (title !== undefined && title.trim() === '') {
+        return res.status(400).json({
+            error: "title cannot be empty"
+        });
+    }
+
+    if (title !== undefined) task.title = title;
+    if (done !== undefined) task.done = done;
+
+    res.json(task);
+});
+
+
+// delete api
+
+app.delete('/tasks/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const index = tasks.findIndex(t => t.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({
+            error: `Task ${id} not found`
+        });
+    }
+
+    tasks.splice(index, 1);
+
+    res.status(204).send();
+});
 
 
 
